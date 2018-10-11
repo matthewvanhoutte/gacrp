174b66cc4ea1e19215ed9579fb99219fc5271570
Matthew Vanhoutte <matthew.vanhoutte2@Imp.net>
2018-10-11 11:42:49 +0100

    Added code from Daniel

--- data_prep.R
+++ data_prep.R
@@ -1,14 +1,50 @@
 #Preprocessing of data
-#Last Modified: 10/10/18
+#Last Modified: 11/10/18
 
+#Required Libraries
 library(data.table)
 library(jsonlite)
+library(tidyverse)
+
+
+# Initialise --------------------------------------------------------------
 
 #Setting working directory to where the Github folder is located
 setwd("~/Documents/Github/garcp")
 
 #Import the data
-train <- fread(file = "~/Documents/Kaggle Comp/train.csv", data.table = FALSE, stringsAsFactors = FALSE)
-test <- fread(file = "~/Documents/Kaggle Comp/test.csv", data.table = FALSE, stringsAsFactors = FALSE)
+tr_name <- "D:/Users/matthew.vanhoutte2/Documents/Kaggle Comp/train.csv"
+te_name <- "D:/Users/matthew.vanhoutte2/Documents/Kaggle Comp/test.csv"
+tr <- read.csv(tr_name)
+te <- read.csv(te_name)
 
-test
+# Functions ---------------------------------------------------------------
+
+#Conversion from JSON to String
+flatten_json <- . %>% 
+  str_c(., collapse = ",") %>% 
+  str_c("[", ., "]") %>% 
+  fromJSON(flatten = T)
+
+parse <- . %>% 
+  bind_cols(flatten_json(.$device)) %>%
+  bind_cols(flatten_json(.$geoNetwork)) %>% 
+  bind_cols(flatten_json(.$trafficSource)) %>% 
+  bind_cols(flatten_json(.$totals)) %>% 
+  select(-device, -geoNetwork, -trafficSource, -totals)
+
+is_na_val <- function(x) x %in% c("not available in demo dataset", "(not set)", 
+                                  "unknown.unknown", "(not provided)")
+
+has_many_values <- function(x) n_distinct(x) > 1
+
+
+
+# Run Time ----------------------------------------------------------------
+
+#Parsing data
+tr <- parse(tr)
+te <- parse(te)
+
+tr <- as.data.frame(tr)
+te <- as.data.frame(te)
