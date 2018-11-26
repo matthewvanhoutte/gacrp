9d3e5c0e719a7f74df809923932616e0ff350488
Matthew <matthew.1717@gmail.com>
2018-11-26 13:36:46 +0000

    more

--- task_3.R
+++ task_3.R
@@ -18,17 +18,34 @@ test <- test %>% summarise(channelGrouping = unique(channelGrouping),
                          visitNumber = unique(visitNumber),
                          visitStartTime = min(visitStartTime),
                          browser = unique(browser)[1],
+                         browserVersion = unique(browserVersion)[1],
                          operatingSystem = unique(operatingSystem)[1],
                          deviceCategory = unique(deviceCategory)[1],
+                         continent = unique(continent)[1],
                          subContinent = unique(subContinent)[1],
                          networkDomain = unique(networkDomain)[1],
                          medium = unique(medium)[1],
                          isTrueDirect = unique(isTrueDirect)[1],
+                         campaignCode = unqiue(campaignCode)[1],
+                         adwordsClickInfo.page = unqiue(adwordsClickInfo.page)[1],
+                         adwordsClickInfo.slot = unqiue(adwordsClickInfo.slot)[1],
+                         adwordsClickInfo.adNetworkType = unqiue(adwordsClickInfo.adNetworkType)[1],
+                         adwordsClickInfo.isVideoAd = unique(adwordsClickInfo.isVideoAd)[1],
+                         visits = max(visits),
                          hits = sum(hits) - 1,
                          pageviews = sum(pageviews) - 1,
+                         bounces = max(bounces),
+                         newVisits = max(newVisits),
                          transactionRevenue = sum(transactionRevenue),
                          day = min(day),
                          month = min(month),
                          year = min(year),
                          fullVisitId = unique(fullVisitId)[1],
                          visitTimeBin12 = max(visitTimeBin12))
+
+# Remove from training set ----
+id_remove <- unique(duplicated_rows$sessionId)
+id_remove <- tr$sessionId %in% id_remove
+tr <- tr[!id_remove,]
+# Add into summary into to training set ----
+tr <- c(tr,test)
