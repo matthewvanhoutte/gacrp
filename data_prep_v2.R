df0891a25994d8d8b537244cea3ea61a51ff18c5
Matthew <matthew.1717@gmail.com>
2018-11-27 22:49:19 +0000

    New data prep for the new data in Kaggle. Currently working on developing a method to try and reduce the data set to keep it computer capable.

---
+++ data_prep_v2.R
@@ -0,0 +1,56 @@
+#Preprocessing of data
+#Last Modified: 11/10/18
+
+#Required Libraries
+library(data.table)
+library(jsonlite)
+library(tidyverse)
+
+
+# Initialise --------------------------------------------------------------
+
+#Setting working directory to where the Github folder is located
+#setwd("~/Documents/Github/garcp")
+setwd("C://Users//user1//Documents//Kaggle")
+
+#Import the data
+#tr_name <- "~/Documents/Kaggle Comp/train.csv"
+#te_name <- "~/Documents/Kaggle Comp/test.csv"
+tr_name <- "train.csv"
+te_name <- "test.csv"
+tr <- read.csv(tr_name)
+te <- read.csv(te_name)
+
+# Functions ---------------------------------------------------------------
+
+#Conversion from JSON to String
+flatten_json <- . %>% 
+  str_c(., collapse = ",") %>% 
+  str_c("[", ., "]") %>% 
+  fromJSON(flatten = T)
+
+parse <- . %>% 
+  bind_cols(flatten_json(.$device)) %>%
+  bind_cols(flatten_json(.$geoNetwork)) %>% 
+  bind_cols(flatten_json(.$trafficSource)) %>% 
+  bind_cols(flatten_json(.$totals)) %>% 
+  select(-device, -geoNetwork, -trafficSource, -totals)
+
+is_na_val <- function(x) x %in% c("not available in demo dataset", "(not set)", 
+                                  "unknown.unknown", "(not provided)")
+
+has_many_values <- function(x) n_distinct(x) > 1
+
+
+
+# Run Time ----------------------------------------------------------------
+
+#Parsing data
+tr <- parse(tr)
+te <- parse(te)
+
+tr <- as.data.frame(tr)
+te <- as.data.frame(te)
+
+
+
